7327762503a1784d459b3ca095e3a14e211208fe
Jim (zhaozheng) Wang <jimzhaozhengwang@gmail.com>
2017-04-27 17:50:39 -0400

    updated for DOTA2 version 7.00+ and changed string comparisons to integer comparisons

--- app/src/main/java/ucalled911/DOTA2TimerAndReminder/ReminderActivity.java
+++ app/src/main/java/ucalled911/DOTA2TimerAndReminder/ReminderActivity.java
@@ -18,9 +18,6 @@ public class ReminderActivity extends AppCompatActivity {
     private MainActivity access = new MainActivity();
     private int second = access.getSecond();
     private int minute = access.getMinute();
-    private String plus_minus_zero = access.getPlus_minus_zero();
-    private String plus = "+";
-    private String minus = "-";
     private long startTime = 0;
     private MediaPlayer the_battle_begins_sound;
     private MediaPlayer stack_sound;
@@ -28,11 +25,12 @@ public class ReminderActivity extends AppCompatActivity {
     private MediaPlayer day_sound;
     private MediaPlayer night_sound;
     private MediaPlayer spawn_sound;
-
-    private String rune_time;
-    private String stack_time;
-    private String spawn_time;
-    private String day_night_time;
+    private int positive_negative;
+    private String plus_minus;
+    private int rune_time;
+    private int stack_time;
+    private int spawn_time;
+    private int day_night_time;
     private SharedPreferences shared_preferences;
 
     private Intent main_activity;
@@ -45,7 +43,7 @@ public class ReminderActivity extends AppCompatActivity {
         public void run() {
             long millisecond = System.currentTimeMillis() - startTime;
             int one_second = (int) (millisecond / 1000);
-            if (one_second == 1){
+            if (one_second == 1) {
                 startTime = System.currentTimeMillis();
                 forward_second_logic();
             }
@@ -61,13 +59,27 @@ public class ReminderActivity extends AppCompatActivity {
         setup();
     }
 
-    public void setup(){
-        
+    private int convert(String string_value) {
+        if (string_value.equals("OFF")) {
+            return -1;
+        } else {
+            return Integer.parseInt(string_value);
+        }
+    }
+
+    public void setup() {
+        if (access.getPlus_minus_zero().equals("+")){
+            positive_negative = 1;
+            plus_minus = "+";
+        }else{
+            positive_negative = 0;
+            plus_minus = "-";
+        }
         shared_preferences = getSharedPreferences(getPackageName() + ".user_settings", Context.MODE_PRIVATE);
-        rune_time = shared_preferences.getString("rune_time", "20");
-        stack_time = shared_preferences.getString("stack_time", "20");
-        spawn_time = shared_preferences.getString("spawn_time", "10");
-        day_night_time = shared_preferences.getString("day_night_time", "30");
+        rune_time = convert(shared_preferences.getString("rune_time", "20"));
+        stack_time = convert(shared_preferences.getString("stack_time", "20"));
+        spawn_time = convert(shared_preferences.getString("spawn_time", "10"));
+        day_night_time = convert(shared_preferences.getString("day_night_time", "30"));
 
         the_battle_begins_sound = MediaPlayer.create(this, R.raw.the_battle_begins);
         stack_sound = MediaPlayer.create(this, R.raw.stack);
@@ -75,10 +87,6 @@ public class ReminderActivity extends AppCompatActivity {
         day_sound = MediaPlayer.create(this, R.raw.day);
         night_sound = MediaPlayer.create(this, R.raw.night);
         spawn_sound = MediaPlayer.create(this, R.raw.spawn);
-
-        display_time();
-        startTime = System.currentTimeMillis();
-        timerHandler.postDelayed(timerRunnable, 0);
 
         minus_button_listener();
         plus_button_listener();
@@ -91,9 +99,13 @@ public class ReminderActivity extends AppCompatActivity {
         this.setTitle("DOTA 2 Timer & Reminder");
 
         main_activity = new Intent(ReminderActivity.this, MainActivity.class);
+
+        display_time();
+        startTime = System.currentTimeMillis();
+        timerHandler.postDelayed(timerRunnable, 0);
     }
 
-    public void minus_button_listener(){
+    public void minus_button_listener() {
         Button minus_button = (Button) findViewById(R.id.minus_button);
         minus_button.setOnClickListener(
                 new Button.OnClickListener() {
@@ -104,7 +116,7 @@ public class ReminderActivity extends AppCompatActivity {
         );
     }
 
-    public void plus_button_listener(){
+    public void plus_button_listener() {
         Button plus_button = (Button) findViewById(R.id.plus_button);
         plus_button.setOnClickListener(
                 new Button.OnClickListener() {
@@ -115,7 +127,7 @@ public class ReminderActivity extends AppCompatActivity {
         );
     }
 
-    public void play_pause_button_listener(){
+    public void play_pause_button_listener() {
         final ToggleButton play_pause_button = (ToggleButton) findViewById(R.id.play_pause_button);
         final String pause = getResources().getString(R.string.r_pause);
         final String play = getResources().getString(R.string.r_play);
@@ -137,76 +149,82 @@ public class ReminderActivity extends AppCompatActivity {
         );
     }
 
-    public void backward_second_logic(){
-        if (plus_minus_zero.equals(plus)){
+    public void backward_second_logic() {
+        if (positive_negative == 1) {
             if (minute == 0 && second == 0) {
-                plus_minus_zero = minus;
+                positive_negative = 0;
+                plus_minus = "-";
                 second = 1;
-            }else if (second == 0){
+            } else if (second == 0) {
                 second = 59;
                 minute--;
-            }else{
+            } else {
                 second--;
             }
-        }else{ // plus_minus_zero.equals(minus)
-            if (second == 59){
-                second = 0;
-                minute++;
-            }
-            else{
-                second++;
-            }
-        }
-        display_time();
-    }
-
-    public void forward_second_logic(){
-        if (plus_minus_zero.equals(plus)) {
+        } else { // negative
             if (second == 59) {
                 second = 0;
                 minute++;
             } else {
                 second++;
             }
-        }else { // plus_minus_zero.equals(minus)
+        }
+        display_time();
+    }
+
+    public void forward_second_logic() {
+        if (positive_negative == 1) {
+            if (second == 59) {
+                second = 0;
+                minute++;
+            } else {
+                second++;
+            }
+        } else { // negative
             if (minute != 0 && second == 0) {
                 second = 59;
                 minute--;
             } else {
+                if (minute == 0 && second == 1){
+                    plus_minus = "+";
+                }
                 second--;
             }
         }
         display_time();
     }
 
-    public void display_time(){
+    public void display_time() {
         String time;
-        if (minute == 0 && second == 0){
+        if (minute == 0 && second == 0) {
             time = String.format("%02d : %02d", minute, second);
             the_battle_begins_sound.start();
-            plus_minus_zero = plus;
-        }else{
-            time = String.format(plus_minus_zero + " %02d : %02d", minute, second);
-            if (plus_minus_zero.equals(minus)){
-                if (rune_time.length() < 3 && minute == 0 && second == Integer.parseInt(rune_time)){
+            positive_negative = 1;
+        } else {
+            time = String.format(plus_minus + " %02d : %02d", minute, second);
+            if (positive_negative == 0) {
+                if (rune_time != -1 && minute == 0 && second == rune_time) {
                     rune_sound.start();
                 }
-            }else{ // plus_minus_zero.equals(plus)
-                if (rune_time.length() < 3 && (minute % 2) != 0 && second == 60 - Integer.parseInt(rune_time)){
+            } else { // positive_negative == 1
+                if (rune_time != -1 && minute % 2 != 0 && second == 60 - rune_time) {
                     rune_sound.start();
                 }
-                if (stack_time.length() < 3 && second == 53 - Integer.parseInt(stack_time)){
+                if (stack_time != -1 && minute % 2 == 0 && second == 54 - stack_time) {
                     stack_sound.start();
                 }
-                if (spawn_time.length() < 3 && (second == 30 - Integer.parseInt(spawn_time) || second == 60 - Integer.parseInt(spawn_time))){
+                if (spawn_time != -1 && (second == 30 - spawn_time || second == 60 - spawn_time)) {
                     spawn_sound.start();
-                    //fix spawn timing array
                 }
-                if (day_night_time.length() < 3 && minute != 0 && minute % 7 == 0 && second == 60 - Integer.parseInt(day_night_time)){
-                    day_sound.start();
-                }else if (day_night_time.length() < 3 && minute != 0 && minute % 3 == 0 && second == 60 - Integer.parseInt(day_night_time)){
-                    night_sound.start();
+                if (day_night_time != -1) { //21
+                    if (minute != 0 && minute % 7 == 0 && second == 60 - day_night_time) {
+                        day_sound.start();
+                    }
+                    if (minute != 0 && minute % 3 == 0 && second == 60 - day_night_time) {
+                        night_sound.start();
+                    }
                 }
+
             }
         }
 
@@ -221,7 +239,7 @@ public class ReminderActivity extends AppCompatActivity {
         startActivity(main_activity);
     }
 
-    public void reset_button_listener(){
+    public void reset_button_listener() {
         Button reset_button = (Button) findViewById(R.id.reset_button);
         reset_button.setOnClickListener(
                 new Button.OnClickListener() {
