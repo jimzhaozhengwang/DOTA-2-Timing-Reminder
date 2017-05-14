2f2db5c50bfb206c4846ac02d1f59c066ed20e31
Jim (zhaozheng) Wang <jimzhaozhengwang@gmail.com>
2017-05-14 14:17:39 -0400

    changed button to floatingActionButton

--- app/src/main/java/ucalled911/DOTA2TimerAndReminder/app/MainActivity.java
+++ app/src/main/java/ucalled911/DOTA2TimerAndReminder/app/MainActivity.java
@@ -1,6 +1,7 @@
 package ucalled911.DOTA2TimerAndReminder.app;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -123,26 +124,25 @@ public class MainActivity extends AppCompatActivity {
     public void start_button_listener(){
         final RadioButton before_button = (RadioButton) findViewById(R.id.before_button);
         // RadioButton after_button = (RadioButton) findViewById(R.id.after_button);
-
-        Button start_button = (Button) findViewById(R.id.start_button);
         final RadioGroup before_after_group = (RadioGroup) findViewById(R.id.before_after_group);
 
-        start_button.setOnClickListener(
-                new Button.OnClickListener() {
-                    public void onClick(View V) {
-                        int before_after_selected_id = before_after_group.getCheckedRadioButtonId();
-                        before_after_button = (RadioButton) findViewById(before_after_selected_id);
-                        if (before_after_button.getId() == before_button.getId()) {
-                            plus_minus_zero = "-";
-                        } else { // before_after_button.getId() == after_button.getId()
-                            plus_minus_zero = "+";
-                        }
-                        Intent reminder_activity = new Intent(MainActivity.this, ReminderActivity.class);
-                        finish();
-                        startActivity(reminder_activity);
-                    }
+        FloatingActionButton fab_start = (FloatingActionButton) findViewById(R.id.fab);
+
+        fab_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int before_after_selected_id = before_after_group.getCheckedRadioButtonId();
+                before_after_button = (RadioButton) findViewById(before_after_selected_id);
+                if (before_after_button.getId() == before_button.getId()) {
+                    plus_minus_zero = "-";
+                } else { // before_after_button.getId() == after_button.getId()
+                    plus_minus_zero = "+";
                 }
-        );
+                Intent reminder_activity = new Intent(MainActivity.this, ReminderActivity.class);
+                finish();
+                startActivity(reminder_activity);
+            }
+        });
     }
 
     public int getSecond(){
